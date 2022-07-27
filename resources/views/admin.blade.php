a5a29d766c2a8dffe4f1500ae37e99d2b8e38a7c
Orzyozzy <sabbytherainbow@gmail.com>
2022-07-28 00:16:54 +0800

    admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,23 +1,123 @@
-@extends('layouts.app')
 
-@section('content')
+@extends('layouts.adminpge')
+@section('section')
+      
+
+    
+    <div class="card-body bg-info text-white mailbox-widget pb-0">
+        <h2 class="text-white pb-3">Pending Booking Dashboard</h2>
+        <ul class="nav nav-tabs custom-tab border-bottom-0 mt-4" id="myTab" role="tablist">
+            
+            <li class="nav-item">
+                <a class="nav-link active" id="inbox-tab" data-toggle="tab" aria-controls="inbox" href="#inbox" role="tab" aria-selected="true">
+                    <span class="d-block d-md-none"><i class="ti-email"></i></span>
+                    <span class="d-none d-md-block"> INBOX</span>
+                </a>
+            </li>
+    
+            <li class="nav-item">
+                <a class="nav-link" id="approved-tab" data-toggle="tab" aria-controls="approve" href="#approve" role="tab" aria-selected="false">
+                    <span class="d-block d-md-none"><i class="ti-export"></i></span>
+                    <span class="d-none d-md-block">APPROVED BOOKINGS</span>
+                </a>
+            </li>
+    
+        </ul>
+    </div>
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    
+
+    <div class="row">
+        <div class="col-md-16">
             <div class="card">
-                <div class="card-header">{{ __('Admin Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+               
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade active show" id="inbox" aria-labelledby="inbox-tab" role="tabpanel">
+                        <div>
+                            <div class="row p-4 no-gutters align-items-center">
+                                <div class="col-sm-12 col-md-6">
+                                    <h3 class="font-light mb-0"><i class="ti-email mr-2"></i># of Bookings</h3>
+                                </div>
+                                <div class="col-sm-12 col-md-6">
+                                    <ul class="list-inline dl mb-0 float-left float-md-right">
+                                        <li class="list-inline-item text-info mr-3">
+                                            <a href="#">
+                                                <button class="btn btn-circle btn-success text-white" href="javascript:void(0)">
+                                                    <i class="fa fa-check"></i>
+                                                </button>
+                                                <span class="ml-2 font-normal text-dark">Accept</span>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item text-danger">
+                                            <a href="#">
+                                                <button class="btn btn-circle btn-danger text-white" href="javascript:void(0)">
+                                                    <i class="fa fa-close"></i>
+                                                </button>
+                                                <span class="ml-2 font-normal text-dark">Decline</span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- Mail list-->
+                            <div class="table-responsive">
+                                <table class="table email-table no-wrap table-hover v-middle mb-0 font-14">
+                                    <tbody>
+                                        <!-- row -->
+                                        <tr>
+                                            <!-- label -->
+                                            <td class="pl-3">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" class="custom-control-input" id="cst1" />
+                                                    <label class="custom-control-label" for="cst1">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <!-- star -->
+                                            <td><i class="fa fa-star text-warning"></i></td>
+                                            <td>
+                                                <span class="mb-0 text-muted">Hritik Roshan</span>
+                                            </td>
+                                            <!-- Message -->
+                                            <td>
+                                                <a class="link" href="javascript: void(0)">
+                                                    <span class="badge badge-pill text-white font-medium badge-danger mr-2">Work</span>
+                                                    <span class="text-dark">Lorem ipsum perspiciatis-</span>
+                                                </a>
+                                            </td>
+                                            <!-- Attachment -->
+                                            <td><i class="fa fa-paperclip text-muted"></i></td>
+                                            <!-- Time -->
+                                            <td class="text-muted">May 13</td>
+                                        </tr>
+                                        <!-- row -->
+                                        <tr>
+                                            <!-- label -->
+                                            
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in as Admin!') }}
+                    </div>
+                    <div class="tab-pane fade" id="approve" aria-labelledby="approved-tab" role="tabpanel">
+                        <div class="row p-3 text-dark">
+                            <div class="col-md-6">
+                                <h3 class="font-light">Lets check profile</h3>
+                                <h4 class="font-light">you can use it with the small code</h4>
+                            </div>
+                            <div class="col-md-6 text-right">
+                                <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a.</p>
+                            </div>
+                        </div>
+                    </div>
+                   
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+    @section('script')
+    
+    @endsection
 @endsection
---
+++ resources/views/layouts/adminpge.blade.php
@@ -0,0 +1,117 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>CarServ - Car Repair HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@600;700&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet"> 
+    
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="{{ asset ('assets/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset ('assets/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset ('assets/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css') }}" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset ('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="{{ asset ('assets/css/style.css') }}" rel="stylesheet">
+ <link href="{{asset('assets/css/adminpage.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('assets/css/toastr.min.css') }}">
+	<script src="{{asset('assets/js/toastr_jquery.min.js') }}"></script>
+	<script src="{{asset('assets/js/toastr.min.js') }}"></script>
+
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
+    
+   
+</head>
+
+   <!-- Spinner Start -->
+   <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+    <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+</div> 
+ <!-- Spinner End -->
+<!-- Navbar Start -->
+<nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+    <a href="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <h2 class="m-0 text-primary"><i class="fa fa-car me-3"></i>STAFF DASHBOARD</h2>
+    </a>
+    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto p-4 p-lg-0">
+         
+                  
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            </div>
+        </ul>
+    </div>
+</nav>
+<!-- Navbar End -->
+
+@yield('section')
+
+
+@yield('script')
+
+
+ <!-- JavaScript Libraries -->
+ <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+ <script src="{{ asset ('assets/lib/wow/wow.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/easing/easing.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/waypoints/waypoints.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/counterup/counterup.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/tempusdominus/js/moment.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/tempusdominus/js/moment-timezone.min.js') }}"></script>
+ <script src="{{ asset ('assets/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js') }}"></script>
+
+ <!-- Template Javascript -->
+ <script src="{{ asset ('assets/js/main.js')}}"></script>
+
+   <!-- Template Javascript for date picker-->
+   <script src="{{ asset ('assets/js/main.js')}}"></script>
+
+
+  </body>
+  </html>
+   
